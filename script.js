615d118943429bc2926e240d3e88271e38cb5c80
JonaCodes <jonathanfarache@gmail.com>
2016-03-29 13:07:45 +0300

    Jona shopping cart code

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 // an array with all of our cart items
 var cart = [];
+var total = 0;
 
 var updateCart = function () {
-  // TODO: finish
+
+	var source = $('#cart-template').html();
+
+	// compile our template html using handlebars
+	var template = Handlebars.compile(source);
+
+	// template can only take an object; reference in HTML with "cart"
+	var newHTML = template({cart});
+
+	// since using {{#each}}, replace the html each time
+	$('.cart-list').html(newHTML);
+
+	$('.total').html(total);
 }
 
-
 var addItem = function (item) {
-  // TODO: finish
+	
+	cart.push(item);
+	total += item.price;
+
+	updateCart();
 }
 
 var clearCart = function () {
-  // TODO: finish
+  cart = [];
+  total = 0;
+  updateCart();
 }
 
 $('.view-cart').on('click', function () {
-  // TODO: hide/show the shopping cart!
+  $(".shopping-cart").toggleClass("show");
 });
 
 $('.add-to-cart').on('click', function () {
-  // TODO: get the "item" object from the page
-  addItem(item);
-  updateCart();
+
+	var name = $(this).closest(".card-container").find(".item").data().name;
+	var price = $(this).closest(".card-container").find(".item").data().price;
+
+	item = {
+		name: name,
+		price: price
+	}
+
+	addItem(item);
+	updateCart();
 });
 
 $('.clear-cart').on('click', function () {
