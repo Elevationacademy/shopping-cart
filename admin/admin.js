719cbb1fcbbdabc93d7d355e742a8ac445aeae3a
Tomer David <younrg@gmail.com>
2016-03-29 23:03:31 +0200

    fixed a bit of indentation and added comments for almost everything

--- admin/admin.js
+++ admin/admin.js
@@ -1,8 +1,10 @@
+// an array with all products, added 2 for tests. !need to impelement push and pull from local storage or remote database!
 var products = [
 {name:'glass',price:68,url:'http://ecx.images-amazon.com/images/I/31AOX24ATKL.jpg'},
 {name:'pencils',price:3,url:'http://ecx.images-amazon.com/images/I/51YFEe%2BCYbL.jpg'}
 ];
 
+//helper function to validate if the add product form was submitted with empty values
 var _validate = function(product){
   var name = true;
   var price = true;
@@ -18,9 +20,9 @@ var _validate = function(product){
     url = false;
   }
   if(!name && !price && !url){
-      $('#name-error').text('You have to enter a name, a price and a url for a photo');
-      $('#name-error').toggleClass('showa');
-      return false;
+    $('#name-error').text('You have to enter a name, a price and a url for a photo');
+    $('#name-error').toggleClass('showa');
+    return false;
   }else if(!name && !price){
     $('#name-error').text('You have to enter a name and a price');
     $('#name-error').toggleClass('showa');
@@ -49,6 +51,7 @@ var _validate = function(product){
   return true;
 }
 
+//add the new product to the array if it's name isn't already taken, otherwise return error
 var addProduct = function (product) {
   var l = products.length;
   if(_validate(product)){
@@ -64,7 +67,7 @@ var addProduct = function (product) {
   }
   return false;
 }
-
+//display all products in the products array in the list display
 var listProducts = function(){
   $('#listContainer').find('ul').empty();
   var l = products.length;
@@ -72,7 +75,7 @@ var listProducts = function(){
     $('#listContainer').find('ul').append('<li><a role="button" class="product">' + products[i].name +'</a></li>')
   }
 }
-
+//display the item card for the clicked product
 var showProduct = function (current) {
   var source = $('#item-card').html();
   var template = Handlebars.compile(source);
@@ -82,13 +85,13 @@ var showProduct = function (current) {
   $('#cardContainer').append(newHtml);
   $('.deletebox').data().id = index;;
 }
-
+//remove a product from the products array
 var removeProduct = function (index){
   products.splice(index,1);
   $('#cardContainer').empty();
   listProducts();
 }
-
+//listener for the product submit button
 $('.submit-product').on('click', function(e) {
   e.preventDefault();
   //getting information from the form
@@ -96,7 +99,6 @@ $('.submit-product').on('click', function(e) {
   var price = $(this).parent().find('#product-price').val();
   var url = $(this).parent().find('#product-img').val();
   var prod = {name:name,price:price,url:url};
-  // console.log($('.showa'));
   $('.showa').toggleClass('showa');
   var success = addProduct(prod);
 
@@ -108,23 +110,23 @@ $('.submit-product').on('click', function(e) {
   } 
 });
 
-
+//listener for a click on the list display button
 $('.list-products').on('click', function(){
   listProducts();
 });
-
+//listener for a click on a product in the list
 $('#listContainer').on('click','.product',function(){
   showProduct($(this));
 });
-
+//listener for the delete button on product card
 $('#cardContainer').on('click', '.deletebox', function () {
   removeProduct($(this).data().id);
 });
-
+//listener for a click on the create product form title, to toggle the view
 $('.create-title').on('click',function(){
   $('.item-form').toggleClass('show');
 });
-
+//listener for a click on the amazon search form title, to toggle the view
 $('.search-title').on('click',function(){
   $('.search-form').toggleClass('show');
 });
--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // an array with all of our cart items
 var cart = [];
+// an array with all products, added 2 for tests. !need to impelement push and pull from local storage or remote database!
 var products = [
 {name:'glass',price:68,url:'http://ecx.images-amazon.com/images/I/31AOX24ATKL.jpg'},
 {name:'pencils',price:3,url:'http://ecx.images-amazon.com/images/I/51YFEe%2BCYbL.jpg'}
 ];
 
-var updateCart = function () {
+//update the cart display
+var updateCart = function() {
+  //templates
   var source1 = $('#cart-item-1').html();
   var template1 = Handlebars.compile(source1);
   var source2 = $('#cart-item-2').html();
   var template2 = Handlebars.compile(source2);
-    $('.cart-list').empty();
-
+  $('.cart-list').empty();
+  //loop through the cart array and display the items with the correct template
   for(i = 0; i < cart.length; i++){
   	if(cart[i].amount > 1){
   		var newHtml = template2(cart[i]);
@@ -20,23 +23,23 @@ var updateCart = function () {
   	}
   	$('.cart-list').append(newHtml);
   }
-  var total = getTotal();
+  //update the cart total display
+  var total = _getTotal();
   $('.total').text(total);
 }
 
-var getTotal = function (){
+//helper method to get the total value of the items in the cart array
+var _getTotal = function(){
 	var total = 0;
-
 	for(i = 0; i < cart.length; i++){
 		var toAdd = cart[i].price * cart[i].amount;
 		total += toAdd;
 	}
 	return total;
 }
-
-var addItem = function (item) {
+//add item to the cart array and display
+var addItem = function(item) {
   var l = cart.length;
-
   for(i = 0; i < l; i++){
   	if(cart[i].name === item.name) {
   		cart[i].amount++
@@ -44,69 +47,62 @@ var addItem = function (item) {
       return;
   	}
   }
-
   cart.push(item);
   updateCart();
 }
-
-var removeItem = function (index) {
+//remove item from the cart array and display
+var removeItem = function(index) {
 	cart.splice(index,1);
 	updateCart();
 }
-
+//clear the cart array and display
 var clearCart = function () {
   cart = [];
   updateCart();
 }
-
-var updateProducts = function () {
+//update the producta display with the current items in the products array !need to impelement push and pull from local storage or remote database!
+var updateProducts = function() {
   var source = $('#item-card').html();
   var template = Handlebars.compile(source);
   $('.row').empty();
-
   for(i = 0; i < products.length; i++){
     var newHtml = template(products[i]);
     $('.row').append(newHtml);
   }
 }
-
-var removeProduct = function (product){
+//remove product from the producta array !need to impelement push and pull from local storage or remote database!
+var removeProduct = function(product){
   var index  = product.index();
   products.splice(index,1);
   updateProducts();
 }
-
-$('.view-cart').on('click', function () {
+//listener for clicks on the show cart toggle button
+$('.view-cart').on('click', function() {
   $('.shopping-cart').toggleClass('show');
 });
-
-// $('.view-manager').on('click' ,function() {
-//   $('.create-item').toggleClass('show');
-// });
-
-$('.row').on('click', '.add-to-cart', function () {
+//listener for the add to cart button
+$('.row').on('click', '.add-to-cart', function() {
   var name = $(this).parent().parent().data().name;
   var price = $($(this).parent()).parent().data().price;
   var item = {name:name,price:price,amount:1};
-
   addItem(item);
   updateCart();
 });
-
-$('.row').on('click', '.deletebox', function () {
+//listener for the delete button on products
+$('.row').on('click', '.deletebox', function() {
   removeProduct($(this).closest('.col-md-4'));
 });
-
-$('.clear-cart').on('click', function () {
+//listener for the clear cart button
+$('.clear-cart').on('click', function() {
   clearCart();
 });
-
+//listener for the individual cart item removal button
 $('.cart-list').on('click','.item-remove', function() {
 	var index = $(this).parent().index();
 	removeItem(index);
 });
 
 
-
+//run update functions at the loading of the page
 updateCart();
 updateProducts();
