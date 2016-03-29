9647b47490bde44fa72261b98fcfb4c4fbfb8ebc
Tomer David <younrg@gmail.com>
2016-03-29 11:18:05 +0200

    added managment of products

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // an array with all of our cart items
 var cart = [];
+var products = [{name:'glass',price:68,url:'http://ecx.images-amazon.com/images/I/31AOX24ATKL.jpg'},{name:'pencils',price:3,url:'http://ecx.images-amazon.com/images/I/51YFEe%2BCYbL.jpg'}];
 
 var updateCart = function () {
-  // TODO: finish
   var source1 = $('#cart-item-1').html();
   var template1 = Handlebars.compile(source1);
   var source2 = $('#cart-item-2').html();
   var template2 = Handlebars.compile(source2);
     $('.cart-list').empty();
+
   for(i = 0; i < cart.length; i++){
   	if(cart[i].amount > 1){
   		var newHtml = template2(cart[i]);
@@ -20,8 +21,9 @@ var updateCart = function () {
   $('.total').text(total);
 }
 
-var getTotal = function(){
+var getTotal = function (){
 	var total = 0;
+
 	for(i = 0; i < cart.length; i++){
 		var toAdd = cart[i].price * cart[i].amount;
 		total += toAdd;
@@ -30,11 +32,11 @@ var getTotal = function(){
 }
 
 var addItem = function (item) {
-  // TODO: finish
   var count = 0;
   var l = cart.length;
+
   for(i = 0; i < l; i++){
-  	if(cart[i].name === item.name){
+  	if(cart[i].name === item.name) {
   		cart[i].amount++
   	}else{
   		count++
@@ -44,30 +46,57 @@ var addItem = function (item) {
   	cart.push(item);
   }
   updateCart();
-  
 }
 
-var removeItem = function(index){
+var removeItem = function (index) {
 	cart.splice(index,1);
 	updateCart();
 }
 
 var clearCart = function () {
-  // TODO: finish
   cart = [];
   updateCart();
 }
 
+var addProduct = function (product) {
+  var l = products.length;
+
+  for(i = 0; i < l; i++){
+    if(products[i].name === product.name) {
+      $('#name-error').text('This name is already taken');
+      $('#name-error').toggleClass('showa');
+      return false;
+    }
+  }
+  products.push(product);
+  return true;
+}
+
+var updateProducts = function () {
+  var source = $('#item-card').html();
+  var template = Handlebars.compile(source);
+  $('.row').empty();
+
+  for(i = 0; i < products.length; i++){
+    var newHtml = template(products[i]);
+    $('.row').append(newHtml);
+  }
+
+}
+
 $('.view-cart').on('click', function () {
-  // TODO: hide/show the shopping cart!
   $('.shopping-cart').toggleClass('show');
 });
 
-$('.add-to-cart').on('click', function () {
-  // TODO: get the "item" object from the page
-  name = $(this).parent().parent().data().name;
-  price = $($(this).parent()).parent().data().price;
-  item = {name:name,price:price,amount:1};
+$('.view-manager').on('click' ,function() {
+  $('.create-item').toggleClass('show');
+});
+
+$('.row').on('click', '.add-to-cart', function () {
+  var name = $(this).parent().parent().data().name;
+  var price = $($(this).parent()).parent().data().price;
+  var item = {name:name,price:price,amount:1};
+
   addItem(item);
   updateCart();
 });
@@ -80,5 +109,26 @@ $('.cart-list').on('click','.item-remove', function() {
 	var index = $(this).parent().index();
 	removeItem(index);
 });
-// update the cart as soon as the page loads!
+
+$('.submit-product').on('click', function() {
+  //getting information from the form
+  var name = $(this).parent().find('#product-name').val();
+  var price = $(this).parent().find('#product-price').val();
+  var url = $(this).parent().find('#product-img').val();
+  var prod = {name:name,price:price,url:url};
+
+  $('.showa').toggleClass('showa');
+  var success = addProduct(prod);
+  updateProducts();
+
+  // cleaning the form
+  if(success){
+    $(this).parent().find('#product-name').val('');
+    $(this).parent().find('#product-price').val('');
+    $(this).parent().find('#product-img').val('');
+  }
+  
+});
+
 updateCart();
+updateProducts();
