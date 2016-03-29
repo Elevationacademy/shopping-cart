bd73d9b383d451a0993b2ddea521a1d4687c13ab
Tomer David <younrg@gmail.com>
2016-03-29 09:35:08 +0200

    done with basic missions

--- script.js
+++ script.js
@@ -3,23 +3,47 @@ var cart = [];
 
 var updateCart = function () {
   // TODO: finish
+  var source = $('#cart-item').html();
+  var template = Handlebars.compile(source);
+    $('.cart-list').empty();
+  for(i = 0; i < cart.length; i++){
+  	var newHtml = template(cart[i]);
+  	$('.cart-list').append(newHtml);
+  }
+  var total = getTotal();
+  $('.total').text(total);
+}
+
+var getTotal = function(){
+	var total = 0;
+	for(i = 0; i < cart.length; i++){
+		total += cart[i].price;
+	}
+	return total;
 }
 
 
 var addItem = function (item) {
   // TODO: finish
+  cart.push(item);
 }
 
 var clearCart = function () {
   // TODO: finish
+  cart = [];
+  updateCart();
 }
 
 $('.view-cart').on('click', function () {
   // TODO: hide/show the shopping cart!
+  $('.shopping-cart').toggleClass('show');
 });
 
 $('.add-to-cart').on('click', function () {
   // TODO: get the "item" object from the page
+  name = $(this).parent().parent().data().name;
+  price = $($(this).parent()).parent().data().price;
+  item = {name:name,price:price};
   addItem(item);
   updateCart();
 });
