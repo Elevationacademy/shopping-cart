106c5f0e158d3c0e7463f143457646af2c23c4d9
Tomer David <younrg@gmail.com>
2016-03-29 22:37:35 +0200

    minor fix for error message on new product form, added new form for future amazon search, made forms hidden at first

--- admin/admin.js
+++ admin/admin.js
@@ -3,18 +3,66 @@ var products = [
 {name:'pencils',price:3,url:'http://ecx.images-amazon.com/images/I/51YFEe%2BCYbL.jpg'}
 ];
 
-var addProduct = function (product) {
-  var l = products.length;
-
-  for(i = 0; i < l; i++){
-    if(products[i].name === product.name) {
-      $('#name-error').text('This name is already taken');
+var _validate = function(product){
+  var name = true;
+  var price = true;
+  var url = true;
+  console.log(product.name)
+  if(product.name === ''){
+    name = false;
+  }
+  if(product.price === ''){
+    price = false;
+  }
+  if(product.url === ''){
+    url = false;
+  }
+  if(!name && !price && !url){
+      $('#name-error').text('You have to enter a name, a price and a url for a photo');
       $('#name-error').toggleClass('showa');
       return false;
-    }
+  }else if(!name && !price){
+    $('#name-error').text('You have to enter a name and a price');
+    $('#name-error').toggleClass('showa');
+    return false;
+  }else if(!price && !url){
+    $('#name-error').text('You have to enter a price and a url for a photo');
+    $('#name-error').toggleClass('showa');
+    return false;
+  }else if(!name && !url){
+    $('#name-error').text('You have to enter a name and a url for a photo');
+    $('#name-error').toggleClass('showa');
+    return false;
+  }else if(!name){
+    $('#name-error').text('You have to enter a name');
+    $('#name-error').toggleClass('showa');
+    return false;
+  }else if(!price){
+    $('#name-error').text('You have to enter a price');
+    $('#name-error').toggleClass('showa');
+    return false;
+  }else if(!url){
+    $('#name-error').text('You have to enter a url for a photo');
+    $('#name-error').toggleClass('showa');
+    return false;
   }
-  products.push(product);
   return true;
+}
+
+var addProduct = function (product) {
+  var l = products.length;
+  if(_validate(product)){
+    for(i = 0; i < l; i++){
+      if(products[i].name === product.name) {
+        $('#name-error').text('This name is already taken');
+        $('#name-error').toggleClass('showa');
+        return false;
+      }
+    }
+    products.push(product);
+    return true;
+  }
+  return false;
 }
 
 var listProducts = function(){
@@ -37,16 +85,18 @@ var showProduct = function (current) {
 
 var removeProduct = function (index){
   products.splice(index,1);
+  $('#cardContainer').empty();
   listProducts();
 }
 
-$('.submit-product').on('click', function() {
+$('.submit-product').on('click', function(e) {
+  e.preventDefault();
   //getting information from the form
   var name = $(this).parent().find('#product-name').val();
   var price = $(this).parent().find('#product-price').val();
   var url = $(this).parent().find('#product-img').val();
   var prod = {name:name,price:price,url:url};
-
+  // console.log($('.showa'));
   $('.showa').toggleClass('showa');
   var success = addProduct(prod);
 
@@ -55,7 +105,7 @@ $('.submit-product').on('click', function() {
     $(this).parent().find('#product-name').val('');
     $(this).parent().find('#product-price').val('');
     $(this).parent().find('#product-img').val('');
-  }  
+  } 
 });
 
 
@@ -64,10 +114,17 @@ $('.list-products').on('click', function(){
 });
 
 $('#listContainer').on('click','.product',function(){
-  console.log('trigger')
   showProduct($(this));
 });
 
 $('#cardContainer').on('click', '.deletebox', function () {
   removeProduct($(this).data().id);
+});
+
+$('.create-title').on('click',function(){
+  $('.item-form').toggleClass('show');
+});
+
+$('.search-title').on('click',function(){
+  $('.search-form').toggleClass('show');
 });
