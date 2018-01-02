1590ccddfc45c8d0095b2f3cfe3b7a79e2a4ae02
iyadsaba <iyadsaba@gmail.com>
2018-01-02 19:39:34 +0200

    add-to-cart, show and hide current cart, clear cart all done,finish independ exe

--- public/script.js
+++ public/script.js
@@ -5,24 +5,48 @@ var updateCart = function () {
   // TODO: Write this function. In this function we render the page.
   // Meaning we make sure that all our cart items are displayed in the browser.
   // Remember to empty the "cart div" before you re-add all the item elements.
+   // clearCart();
+    var $cartList = $('.cart-list');
+    var $total = $('.total');
+    //clearing data before render back the cart array 
+    $total.text(0);
+    $cartList.contents().remove();
+    //using handlebars templates to add items to the cart
+    var source = $("#add-item-to-cart").html();
+    var template = Handlebars.compile(source);
+    var totalAmount = 0;
+    for(var i=0;i<cart.length;i++){
+        var newHtml = template(cart[i]);
+        totalAmount += cart[i].price;
+        $cartList.append(newHtml);
+    }
+    $total.text(totalAmount);
 }
 
-
 var addItem = function (item) {
-  // TODO: Write this function. Remember this function has nothing to do with display. 
-  // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
+  cart.push(item);
 }
 
 var clearCart = function () {
-  // TODO: Write a function that clears the cart ;-)
+    //removing all items from the cart 
+    $('.cart-list').contents().remove();
+    //reset the total amount 
+    $('.total').text("0");
+    //clearing data from the array the easy way []
+    cart = [];
 }
 
 $('.view-cart').on('click', function () {
-  // TODO: hide/show the shopping cart!
+    $('.shopping-cart').toggleClass('show');
 });
 
 $('.add-to-cart').on('click', function () {
-  // TODO: get the "item" object from the page
+    //select item from the DOM
+    var selectedItem =$(this).closest('.item');
+    //creating item object to add it to the cart 
+    var item = {};
+    item.name = selectedItem.data("name");
+    item.price = selectedItem.data("price");
   addItem(item);
   updateCart();
 });
