a262c65c7ddb7d3eabf78b5e9adb060bdb32b1a1
Yoni <yonatani.mat@gmail.com>
2018-01-02 16:21:00 +0200

    pretty print

--- public/script.js
+++ public/script.js
@@ -2,11 +2,8 @@
 var cart = [];
 
 var updateCart = function () {
-  // TODO: Write this function. In this function we render the page.
-  // Meaning we make sure that all our cart items are displayed in the browser.
-  // Remember to empty the "cart div" before you re-add all the item elements.
   $('.cart-list').empty();
-var total = 0;
+  var total = 0;
   var source = $('#cart-item').html();
   var template = Handlebars.compile(source);
   for (var i = 0; i < cart.length; i++) {
@@ -17,11 +14,7 @@ var total = 0;
   $('.total').text(total);
 }
 
-
-
 var addItem = function (item) {
-  // TODO: Write this function. Remember this function has nothing to do with display. 
-  // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
   var newItem = {
     name: item.children('.card').data().name,
     price: item.children('.card').data().price
@@ -30,18 +23,15 @@ var addItem = function (item) {
 }
 
 var clearCart = function () {
-  // TODO: Write a function that clears the cart ;-)
   cart = [];
   updateCart();
 }
 
 $('.view-cart').on('click', function () {
-  // TODO: hide/show the shopping cart!
   $('.shopping-cart').toggle();
 });
 
 $('.add-to-cart').on('click', function () {
-  // TODO: get the "item" object from the page
   var item = $(this).parent().closest('.card-container');
   addItem(item);
   updateCart();
